89e825fcc356215b9c735a5c42d78f5ef0eb3a9f
Diego Miranda Olavarría <dhmiraola@gmail.com>
2025-10-19 00:29:04 -0300

    app.py

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, request
+from flask import Flask, send_file, request
+
 app = Flask(__name__)
 
-@app.route("/callback")
+@app.route('/')
+def home():
+    return "✅ Servidor Flask funcionando correctamente en Vercel"
+
+@app.route('/callback')
 def callback():
-    code = request.args.get("code")
-    return f"✅ TikTok devolvió el código: {code}", 200
+    code = request.args.get('code')
+    if code:
+        return f"✅ TikTok devolvió el código: {code}"
+    else:
+        return "❌ No se recibió ningún código desde TikTok"
+
+@app.route('/terms')
+def terms():
+    return send_file("terms.html")
+
+@app.route('/privacy')
+def privacy():
+    return send_file("privacy.html")
 
 if __name__ == "__main__":
     app.run()
